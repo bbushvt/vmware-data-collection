1a18a3369d0a3f08115b593f648f8ce6c40859fc
Bryan Bush <bbushvt@gmail.com>
2020-09-25 21:51:15 -0400

    major changes to powershell script
    - Fixed bug with ID being used for name
    - Added anonomization tables for datacenter and cluster names
    	A table is created mapping real cluster names to "cluster<index>"
    	A table is created mapping the real data center names to "datacenter<index>"
    - Changed checks for $vcenter, $username, and $password to compare to an empty string vs null
    - Added default $metricDays value of 7 instead of 0
    - initialized $vmstat as an empty hastable vs what was there before
    - Set $allData.vms to be the $allvms hashtable vs selecting specific fields.  Should be easier to maintain
    - Set $allData.hosts to be the allhosts hashtable vs selecting specific fields.  Should be easier to maintain
    - Fixed the id field of a VM to only show the vm-xxxx part.  This is to match the output format used in akasia
    - Removed the anonomization of the id field, isn't necessary
    - Changed ID to VMID
    - Added the HostID field, obtained from the $vm object
    - Added the Cluster field, obtained using the Get-VMHost call
    - Added the Datacenter field, obtained using the Get-Datacenter call
    - Changed HarddiskGB to StorageAllocated
    - Change method of getting StorageAllocated to grab it from the $vm object instead of making another call to vcenter
    - Added StorageUsed, getting the info from the $vm object
    - Fixed bug when getting GuestOS.  When powered off, $vm.Guest.OSFullname would return '' or null when outputed to JSON
    - Added VDiskCount which is the number of virtual disks attached to the VM
    - Added HostID to the hosts, this will allow for mapping a VM to a host even when anonomized 
    - Updated JSON Format

--- vmware-data-collection.ps1
+++ vmware-data-collection.ps1
@@ -1,4 +1,4 @@
-param ([string] $vcenter, [string] $username, [String] $password, [int] $metricDays, [switch] $csv, [switch] $anon)
+param ([string] $vcenter, [string] $username, [string] $password, [int] $metricDays = 7, [switch] $csv, [switch] $anon)
 
 function ExitWithCode {
     param
@@ -10,27 +10,50 @@ function ExitWithCode {
     exit
 }
 
-Function IIf($If, $Right, $Wrong) {If ($If) {$Right} Else {$Wrong}}
+$anonTables = @{
+    cluster    = @{
+        table  = @{}
+        index  = 0
+        prefix = "cluster"
+    }
+    datacenter = @{
+        table  = @{}
+        index  = 0
+        prefix = "datacenter"
+    }
+}
+Function LookupName($table, $name) {
+    if ($anon) {
+        if (-NOT $table.table.ContainsKey($name)) {
+            $table.table.add($name, $table.prefix + $table.index)
+            $table.index += 1
+        }
+        return ($table.table)[$name]
+    }
+    else {
+        return $name
+    }
+}
 
-Function Get-StringHash([String] $data)
-{
+Function IIf($If, $Right, $Wrong) { If ($If) { $Right } Else { $Wrong } }
+
+Function Get-StringHash([String] $data) {
     $sha1 = New-Object System.Security.Cryptography.SHA1CryptoServiceProvider
     $hashByteArray = $sha1.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($data))
-    foreach($byte in $hashByteArray)
-    {
-      $result += "{0:X2}" -f $byte
+    foreach ($byte in $hashByteArray) {
+        $result += "{0:X2}" -f $byte
     }
     return $result;
 }
 
 # Check to make sure we have all the parameters we need
-if ($null -eq $vcenter) {
+if ('' -eq $vcenter) {
     $vcenter = Read-Host "Hostname or IP address for vCenter"
 }
-if ($null -eq $username) {
+if ('' -eq $username) {
     $username = Read-Host "vCenter Username"
 }
-if ($null -eq $password) {
+if ('' -eq $password) {
     $ss_password = Read-Host "vCenter Password" -AsSecureString
     $password = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($ss_password))
 }
@@ -47,7 +70,38 @@ if (0 -eq $?) {
 }
 
 Write-Output "BEGIN_DATA_PARSE_SECTION"
-$allData = "" | Select-Object vms, hosts
+#$allData = "" | Select-Object vms, hosts
+$allData = @{
+    VMware = @{
+        Clusters = @()
+        Hosts    = @()
+    }
+}
+
+Function Add-VM($vmdata) {
+    $found = $false
+    foreach ($cluster in $allData.VMware.Clusters) {        
+        if ($cluster.Name -eq $vmdata.Cluster) {
+            $cluster.vms += $vmdata
+            $found = $true
+        }
+    }
+
+    if ($found -eq $false) {
+        $newCluster = @{
+            Name = $vmdata.Cluster
+            vms  = @()
+        }
+        $newCluster.vms += $vmdata
+        $allData.VMware.Clusters += $newCluster
+    }
+}
+
+Function Add-Host($hostdata) {
+    $allData.VMware.Hosts += $hostdata
+}
+
+
 $allvms = @()
 $allhosts = @()
 
@@ -55,9 +109,12 @@ $hosts = Get-VMHost
 $vms = Get-Vm
 
 foreach ($vmHost in $hosts) {
-    $hoststat = "" | Select-Object HostName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin, NumCpu, CpuTotalGhz
+    # TODO - BCBUSH - Remove me
+    #$hoststat = "" | Select-Object HostName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin, NumCpu, CpuTotalGhz
+    $hoststat = @{}
 
     $hoststat.HostName = (IIF $anon (Get-StringHash $vmHost.name) $vmHost.name)
+    $hoststat.HostID = $vmHost.ID.substring(11)
   
     $statcpu = Get-Stat -Entity ($vmHost)-start (get-date).AddDays(-$metricDays) -Finish (Get-Date)-MaxSamples 10 -stat cpu.usage.average
     $statmem = Get-Stat -Entity ($vmHost)-start (get-date).AddDays(-$metricDays) -Finish (Get-Date)-MaxSamples 10 -stat mem.usage.average
@@ -73,24 +130,32 @@ foreach ($vmHost in $hosts) {
     $hoststat.MemMin = [math]::round($mem.Minimum, 2)
     $hoststat.CpuTotalGhz = [math]::round($vmHost.CpuTotalMhz / 1000, 2)
     $hoststat.NumCpu = $vmHost.NumCpu
-    $allhosts += $hoststat
+    #$allhosts += $hoststat
+    Add-Host $hoststat
 }
 
 foreach ($vm in $vms) {
-    $vmstat = "" | Select-Object Id, VmName, GuestOS, PowerState, NumCPUs, CpuGhz, MemoryGB, HarddiskGB, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin
+    # TODO - BCBUSH - Remove me
+    #$vmstat = "" | Select-Object Id, VmName, GuestOS, PowerState, NumCPUs, CpuGhz, MemoryGB, HarddiskGB, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin
+    $vmstat = @{}
     
-    $vmstat.ID = (IIF $anon (Get-StringHash $vm.ID) $vm.ID)
-    $vmstat.VmName = (IIF $anon (Get-StringHash $vm.ID) $vm.ID)
+    $vmstat.VMID = $vm.ID.substring(15)
+    $vmstat.HostID = $vm.VmHostId.substring(11)
+    $vmstat.VmName = (IIF $anon (Get-StringHash $vm.Name) $vm.Name)
+    $vmstat.Cluster = LookupName $anonTables.cluster (Get-VMHost -VM IBMCli).parent.name
+    $vmstat.Datacenter = LookupName $anonTables.datacenter (Get-Datacenter -VM IBMCli).Name
 
-    $vmstat.GuestOS = $vm.Guest.OSFullname
-    $vmstat.Powerstate = $vm.powerstate
+    $vmstat.GuestOS = IIF $vm.Guest.OSFullname.length $vm.Guest.OSFullname (Get-View -viewtype VirtualMachine -filter @{"Name" = $vm.Name }).Config.GuestFullName
+    $vmstat.Powerstate = $vm.PowerState
     $vmstat.NumCPUs = $vm.NumCPU
     $vmstat.MemoryGB = $vm.MemoryGB
     $vmstat.CpuGhz = [math]::round($vm.NumCPU * $vmHost.CpuTotalMhz / $vmHost.NumCpu / 1000, 2)
+    $vmstat.VDiskCount = (Get-HardDisk $vm.Name).count
 
-    $vmstat.HarddiskGB = [math]::round((Get-HardDisk -VM $vm | Measure-Object -Sum CapacityGB).Sum, 2)
+    $vmstat.StorageAllocated = [math]::round($vm.ProvisionedSpaceGB, 2)
+    $vmstat.StorageUsed = [math]::round($vm.UsedSpaceGB, 2)
     
-    if ($vmstat.Powerstate) {
+    if ($vm.PowerState) {
         $statcpu = Get-Stat -Entity ($vm)-start (get-date).AddDays(-$metricDays) -Finish (Get-Date)-MaxSamples 10 -stat "cpu.usage.average"
         $statmem = Get-Stat -Entity ($vm)-start (get-date).AddDays(-$metricDays) -Finish (Get-Date)-MaxSamples 10 -stat "mem.usage.average"
   
@@ -112,16 +177,21 @@ foreach ($vm in $vms) {
         $vmstat.MemMin = 0.0
     }
     
-    $allvms += $vmstat
+    #$allvms += $vmstat
+    Add-VM $vmstat
 }
-$allData.vms = $allvms | Select-Object ID, VmName, GuestOS, PowerState, NumCPUs, CPUGhz, MemoryGB, HarddiskGB, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin
-$allData.hosts = $allhosts | Select-Object HostName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin, NumCpu, CpuTotalGhz 
+# TODO - BCBUSH - Remove me
+#$allData.vms = $allvms | Select-Object ID, VmName, GuestOS, PowerState, NumCPUs, CPUGhz, MemoryGB, HarddiskGB, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin
+#$allData.hosts = $allhosts | Select-Object HostName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin, NumCpu, CpuTotalGhz 
+#$allData.vms = $allvms
+#$allData.hosts = $allhosts 
+
 
 if ($csv) {
     $allhosts | Select-Object HostName, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin | Export-Csv "Hosts.csv" -noTypeInformation
     $allvms | Select-Object VmName, PowerState, NumCPUs, MemoryGB, HarddiskGB, MemMax, MemAvg, MemMin, CPUMax, CPUAvg, CPUMin | Export-Csv "VMs10.csv" -noTypeInformation
 }
 
-Write-Output $allData | ConvertTo-Json
+Write-Output $allData | ConvertTo-Json -Depth 50
 
 Exit 0
